666a945d338d8231dbc88398cbbc8d5da1b9ca09
jsaarine <juuso.saarinen@gmail.com>
2020-07-31 16:00:05 +0300

    Disable auto updates

--- index.php
+++ index.php
@@ -12,4 +12,13 @@ if(!defined('ABSPATH')) exit; // Exit if accessed directly
 include dirname(__FILE__).'/sample-block/block.php';
 // include dirname(__FILE__).'/sample-block2/block.php';
 
+// Disable auto updates
+add_filter('plugin_auto_update_setting_html', function($html, $plugin_file, $plugin_data) {
+	if('fiesta-blocks/index.php' === $plugin_file ) {
+		$html = __('Auto-updates are not available for this plugin.');
+	}
+
+	return $html;
+}, 10, 3);
+
 ?>
